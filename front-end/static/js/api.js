c4283d401fb8702df3ff38512bf5530f318de636
pgm-meesakveld <mees.akveld@student.arteveldehs.be>
2024-01-14 19:58:44 +0100

    Code cleanup

--- front-end/static/js/api.js
+++ front-end/static/js/api.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:1212/api/recipes'
+const API_URL = 'http://localhost:1212/api/recipes/'
 
 async function getAllRecipes(callback, query = "") {
     const response = await fetch(API_URL + `?query=${query}`);
@@ -7,19 +7,19 @@ async function getAllRecipes(callback, query = "") {
 }
 
 async function getCategories(callback) {
-    const response = await fetch(API_URL + '/categories');
+    const response = await fetch(API_URL + 'categories');
     const data = await response.json();
     callback(data);
 }
 
 async function getDifficulties(callback) {
-    const response = await fetch(API_URL + '/difficulty');
+    const response = await fetch(API_URL + 'difficulty');
     const data = await response.json();
     callback(data);
 }
 
 async function getIngredients(callback) {
-    const response = await fetch(API_URL + '/ingredients');
+    const response = await fetch(API_URL + 'ingredients');
     const data = await response.json();
     callback(data);
 }
